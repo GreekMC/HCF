1c47e0a93f3c8b31a966108865893949979dfeee
JkqzDev <espulefjuan13@gmail.com>
2022-05-03 03:41:54 -0300

    Create hcf. We need continue

---
+++ src/juqn/hcf/faction/FactionManager.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace juqn\hcf\faction;
+
+use juqn\hcf\faction\command\FactionCommand;
+use juqn\hcf\HCFLoader;
+use juqn\hcf\player\Player;
+
+/**
+ * Class FactionManager
+ * @package juqn\hcf\faction
+ */
+class FactionManager
+{
+    
+    /** @var Faction[] */
+    private array $factions = [];
+    /** @var FactionInvite[] */
+    private array $invites = [];
+    
+    /**
+     * FactionManager construct.
+     */
+    public function __construct()
+    {
+        # Register command
+        HCFLoader::getInstance()->getServer()->getCommandMap()->register('HCF', new FactionCommand());
+        # Register event handler
+        HCFLoader::getInstance()->getServer()->getPluginManager()->registerEvents(new FactionListener(), HCFLoader::getInstance());
+        # Register factions
+        foreach (HCFLoader::getInstance()->getProvider()->getFactions() as $name => $data)
+            $this->createFaction($name, $data);
+    }
+    
+    /**
+     * @return Faction[]
+     */
+    public function getFactions(): array
+    {
+        return $this->factions;
+    }
+    
+    /**
+     * @param string $xuid
+     * @return FactionInvite[]|null
+     */
+    public function getInvites(string $xuid): ?array
+    {
+        return $this->invites[$xuid] ?? null;
+    }
+    
+    /**
+     * @param string $name
+     * @return Faction|null
+     */
+    public function getFaction(string $name): ?Faction
+    {
+        return $this->factions[$name] ?? null;
+    }
+    
+    /**
+     * @param string $name
+     * @param array $data
+     */
+    public function createFaction(string $name, array $data): void
+    {
+        $this->factions[$name] = new Faction($name, $data);
+    }
+    
+    /**
+     * @param Player $player
+     * @param Player $target
+     */
+    public function createInvite(Player $player, Player $target): void
+    {
+        $this->invites[$target->getXuid()][$player->getName()] = new FactionInvite($player, $player->getSession()->getFaction(), time() + 60);
+    }
+    
+    /**
+     * @param Player $player
+     */
+    public function removeInvites(Player $player): void
+    {
+        unset($this->invites[$player->getXuid()]);
+    }
+    
+    /**
+     * @param string $name
+     */
+    public function removeFaction(string $name): void
+    {
+        unset($this->factions[$name]);
+        @unlink(HCFLoader::getInstance()->getDataFolder() . 'factions' . DIRECTORY_SEPARATOR . $name . '.json');
+    }
+}
---
+++ src/juqn/hcf/faction/command/subcommand/HomeSubCommand.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace juqn\hcf\faction\command\subcommand;
+
+use juqn\hcf\faction\command\FactionSubCommand;
+use juqn\hcf\HCFLoader;
+use juqn\hcf\player\Player;
+
+use pocketmine\command\CommandSender;
+use pocketmine\scheduler\ClosureTask;
+use pocketmine\scheduler\TaskHandler;
+use pocketmine\utils\TextFormat;
+
+/**
+ * Class HomeSubCommand
+ * @package juqn\hcf\faction\command\subcommand
+ */
+class HomeSubCommand implements FactionSubCommand
+{
+    
+    /**
+     * @param CommandSender $sender
+     * @param array $args
+     */
+    public function execute(CommandSender $sender, array $args): void
+    {
+        if (!$sender instanceof Player)
+            return;
+        
+        if ($sender->getSession()->getFaction() === null) {
+            $sender->sendMessage(TextFormat::colorize('&cYou don\' have a faction'));
+            return;
+        }
+        $faction = HCFLoader::getInstance()->getFactionManager()->getFaction($sender->getSession()->getFaction());
+        
+        if ($faction->getHome() === null) {
+            $sender->sendMessage(TextFormat::colorize('&cYour faction has no home'));
+            return;
+        }
+        
+        $sender->getSession()->addCooldown('faction.teleport.home',  '&l&1Home: &r&7', 15);
+        
+        $xuid = $sender->getXuid();
+        $position = $sender->getPosition();
+        $handler = null;
+        $handler = HCFLoader::getInstance()->getScheduler()->scheduleRepeatingTask(new ClosureTask(function () use (&$handler, &$sender, &$xuid, &$position): void {
+            $s = HCFLoader::getInstance()->getSessionManager()->getSession($xuid);
+            $faction = HCFLoader::getInstance()->getFactionManager()->getFaction($s->getFaction());
+            
+            if (!$sender->isOnline()) {
+                if ($s->getCooldown('faction.teleport.home') !== null) $s->removeCooldown('faction.teleport.home');
+                $handler->cancel();
+                return;
+            }
+            
+            if ($faction === null) {
+                if ($s->getCooldown('faction.teleport.home') !== null) $s->removeCooldown('faction.teleport.home');
+                $handler->cancel();
+                return;
+            }
+            
+            if ($position->distance($sender->getPosition()) > 2) {
+                if ($s->getCooldown('faction.teleport.home') !== null) $s->removeCooldown('faction.teleport.home');
+                $handler->cancel();
+                return;
+            }
+            
+            if ($sender->getSession()->getCooldown('spawn.tag') !== null) {
+                if ($s->getCooldown('faction.teleport.home') !== null) $s->removeCooldown('faction.teleport.home');
+                $handler->cancel();
+                return;
+            }
+            
+            if ($faction->getHome() === null) {
+                if ($s->getCooldown('faction.teleport.home') !== null) $s->removeCooldown('faction.teleport.home');
+                $handler->cancel();
+                return;
+            }
+            
+            if ($sender->getSession()->getCooldown('faction.teleport.home') === null) {
+                $sender->teleport($faction->getHome());
+                $handler->cancel();
+            }
+        }), 20);
+    }
+}
