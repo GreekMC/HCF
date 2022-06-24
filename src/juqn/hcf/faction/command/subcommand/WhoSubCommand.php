5b4bf7588685e0ba617ea7fe7ce0ebfc2a1464c5
itoozh <74574377+itoozh@users.noreply.github.com>
2022-06-24 09:12:52 -0500

    fix f who

--- src/juqn/hcf/faction/command/subcommand/WhoSubCommand.php
+++ src/juqn/hcf/faction/command/subcommand/WhoSubCommand.php
@@ -1,79 +1,80 @@
-<?php
+    <?php
 
-declare(strict_types=1);
+    declare(strict_types=1);
 
-namespace juqn\hcf\faction\command\subcommand;
+    namespace juqn\hcf\faction\command\subcommand;
 
-use juqn\hcf\faction\command\FactionSubCommand;
-use juqn\hcf\faction\Faction;
-use juqn\hcf\HCFLoader;
-use juqn\hcf\player\Player;
-use pocketmine\command\CommandSender;
-use pocketmine\utils\TextFormat;
+    use juqn\hcf\faction\command\FactionSubCommand;
+    use juqn\hcf\faction\Faction;
+    use juqn\hcf\HCFLoader;
+    use juqn\hcf\player\Player;
+    use pocketmine\command\CommandSender;
+    use pocketmine\utils\TextFormat;
 
-class WhoSubCommand implements FactionSubCommand
-{
-
-    /**
-     * @param CommandSender $sender
-     * @param array $args
-     */
-    public function execute(CommandSender $sender, array $args): void
+    class WhoSubCommand implements FactionSubCommand
     {
-        if (!$sender instanceof Player)
-            return;
-        $faction = null;
-        
-        if (!isset($args[0])) {
-            if ($sender->getSession()->getFaction() === null) {
-                $sender->sendMessage(TextFormat::colorize('&cYou don\'t have faction'));
-                return;
-            }
-            $faction = $sender->getSession()->getFaction();
-        } else {
-            $target = $sender->getServer()->getPlayerByPrefix($args[0]);
 
-            if ($target instanceof Player) {
-                if ($target->getSession()->getFaction() === null) {
-                    $sender->sendMessage(TextFormat::colorize('Player dont have faction'));
+        /**
+         * @param CommandSender $sender
+         * @param array $args
+         */
+        public function execute(CommandSender $sender, array $args): void
+        {
+            if (!$sender instanceof Player)
+                return;
+            $faction = null;
+
+            if (!isset($args[0])) {
+                if ($sender->getSession()->getFaction() === null) {
+                    $sender->sendMessage(TextFormat::colorize('&cYou don\'t have faction'));
                     return;
                 }
-                $faction = $target->getSession()->getFaction();
+                $faction = $sender->getSession()->getFaction();
             } else {
-                if (HCFLoader::getInstance()->getFactionManager()->getFaction($args[0])) {
-                    $faction = $args[0];
+                $target = $sender->getServer()->getPlayerByPrefix($args[0]);
+
+                if ($target instanceof Player) {
+                    if ($target->getSession()->getFaction() === null) {
+                        $sender->sendMessage(TextFormat::colorize('Player dont have faction'));
+                        return;
+                    }
+                    $faction = $target->getSession()->getFaction();
+                } else {
+                    if (HCFLoader::getInstance()->getFactionManager()->getFaction($args[0])) {
+                        $faction = $args[0];
+                    }
                 }
             }
-        }
 
-        if ($faction === null) {
-            $sender->sendMessage(TextFormat::colorize('&cNo faction found'));
-            return;
+            if ($faction === null) {
+                $faction = $sender->getSession()->getFaction();
+                $sender->sendMessage(TextFormat::colorize('&cNo faction found'));
+                return;
+            }
+            $message = '&7&m--------------------------' . PHP_EOL;
+            $message .= '&9' . (HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getName()). ' &7[' . count(HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getOnlineMembers()) . '/' . count(HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getMembers()) . '] &3- &eHQ: &f' . (HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getHome() !== null ? 'X: ' . HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getHome()->getFloorX() . ' Z: ' . HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getHome()->getFloorZ() : 'Not set ');
+            $message .= '&eLeader: ' . implode(', ', array_map(function ($session) {
+                return $session->getName();
+            }, HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getMembersByRole(Faction::LEADER))) . PHP_EOL;
+            $message .= '&eColeaders: ' . implode(', ', array_map(function ($session) {
+                return $session->getName();
+            }, HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getMembersByRole(Faction::CO_LEADER))) . PHP_EOL;
+            $message .= '&eCaptains: ' . implode(', ', array_map(function ($session) {
+                return $session->getName();
+            }, HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getMembersByRole(Faction::CAPTAIN))) . PHP_EOL;
+            $message .= '&eMembers: ' . implode(', ', array_map(function ($session) {
+                return $session->getName();
+            }, HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getMembersByRole(Faction::MEMBER))) . PHP_EOL;
+            $message .= '&eBalance: &9$' . HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getBalance() . PHP_EOL;
+            $message .= '&eDeaths until Raidable: ' . (HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getDtr() >= HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getMaxDtr() ? '&a' : (HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getDtr() <= 0.00 ? '&c' : '&e')) . round(HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getDtr(), 2) . '■' . PHP_EOL;
+
+            if (HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getTimeRegeneration() !== null) {
+                $message .= '&eTime Until Regen: &9' . gmdate('H:i:s', HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getTimeRegeneration()) . PHP_EOL;
+            }
+            $message .= '&ePoints: &c' . HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getPoints() . PHP_EOL;
+            $message .= '&eKoTH Captures: &c' . HCFLoader::getInstance()->getFactionManager()->getFaction($faction)->getKothCaptures() . PHP_EOL;
+            $message .= '&7&m--------------------------';
+
+            $sender->sendMessage(TextFormat::colorize($message));
         }
-        $message = '&7&m--------------------------' . PHP_EOL;
-        $message .= '&9' . ($faction->getName()). ' &7[' . count($faction->getOnlineMembers()) . '/' . count($faction->getMembers()) . '] &3- &eHQ: &f' . ($faction->getHome() !== null ? 'X: ' . $faction->getHome()->getFloorX() . ' Z: ' . $faction->getHome()->getFloorZ() : 'Not set ');
-        $message .= '&eLeader: ' . implode(', ', array_map(function ($session) {
-            return $session->getName();
-        }, $faction->getMembersByRole(Faction::LEADER))) . PHP_EOL;
-        $message .= '&eColeaders: ' . implode(', ', array_map(function ($session) {
-            return $session->getName();
-        }, $faction->getMembersByRole(Faction::CO_LEADER))) . PHP_EOL;
-        $message .= '&eCaptains: ' . implode(', ', array_map(function ($session) {
-            return $session->getName();
-        }, $faction->getMembersByRole(Faction::CAPTAIN))) . PHP_EOL;
-        $message .= '&eMembers: ' . implode(', ', array_map(function ($session) {
-            return $session->getName();
-        }, $faction->getMembersByRole(Faction::MEMBER))) . PHP_EOL;
-        $message .= '&eBalance: &9$' . $faction->getBalance() . PHP_EOL;
-        $message .= '&eDeaths until Raidable: ' . ($faction->getDtr() >= $faction->getMaxDtr() ? '&a' : ($faction->getDtr() <= 0.00 ? '&c' : '&e')) . round($faction->getDtr(), 2) . '■' . PHP_EOL;
-        
-        if ($faction->getTimeRegeneration() !== null) {
-            $message .= '&eTime Until Regen: &9' . gmdate('H:i:s', $faction->getTimeRegeneration()) . PHP_EOL;
-        }
-        $message .= '&ePoints: &c' . $faction->getPoints() . PHP_EOL;
-        $message .= '&eKoTH Captures: &c' . $faction->getKothCaptures() . PHP_EOL;
-        $message .= '&7&m--------------------------';
-        
-        $sender->sendMessage(TextFormat::colorize($message));
     }
-}
