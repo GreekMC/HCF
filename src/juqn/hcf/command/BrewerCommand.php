00b00479a533dccd1debd0875d72bd654d29aee2
Juan E. Espulef <99534798+JkqzDev@users.noreply.github.com>
2022-06-26 12:22:07 -0300

    Some stuff

--- src/juqn/hcf/command/BrewerCommand.php
+++ src/juqn/hcf/command/BrewerCommand.php
@@ -36,6 +36,16 @@ class BrewerCommand extends Command
         
         if (!$this->testPermission($sender))
             return;
+
+        if ($sender->getSession()->getFaction === null) {
+            $sender->sendMessage(TextFormat::colorize('&cYou can\'t use this command '));
+            return;
+        }
+        
+        if ($sender->getSession()->getFaction() !== $sender->getCurrentClaim()) {
+            $sender->sendMessage(TextFormat::colorize('&cYou can\'t use this command '));
+            return;
+        }
         
         if ($sender->getSession()->getCooldown('brewer.cooldown') !== null) {
             $sender->sendMessage(TextFormat::colorize('&cYou have cooldown to use this'));
